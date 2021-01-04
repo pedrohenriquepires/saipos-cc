7f0d3bba3791cf9ce45b9fe682f3e02ef3e3c48f
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2021-01-04 14:05:01 -0300

    update tests

--- api/src/modules/tasks/__tests__/task-list-controller.spec.ts
+++ api/src/modules/tasks/__tests__/task-list-controller.spec.ts
@@ -2,6 +2,8 @@ import request from 'supertest'
 import { database } from '../../../datasources/knex'
 import { expressTestApp } from '../../../__tests__/utils/get-express-test-app'
 
+jest.setTimeout(30000);
+
 describe('Task List Controller', () => {
   beforeEach(async () => {
     await database('tasks').truncate()
@@ -15,20 +17,20 @@ describe('Task List Controller', () => {
     const response = await request(expressTestApp).get('/tasks').send()
 
     expect(response.status).toBe(200)
-    expect(response.body).toEqual([])
+    expect(response.body).toEqual({done: [], pending: []})
   })
 
   it('should return a list with 1 tasks', async () => {
     const TASK = {
       description: 'Lorem ipsum',
       ownerName: 'Teste',
-      ownerEmail: 'fake@test.com'
+      ownerEmail: 'fake@me.com'
     }
     
     const createdTask = (await request(expressTestApp).post('/tasks').send(TASK)).body
     const response = await request(expressTestApp).get('/tasks').send()
 
     expect(response.status).toBe(200)
-    expect(response.body).toEqual([createdTask])
+    expect(response.body).toEqual({done: [], pending: [createdTask]})
   })
 })
